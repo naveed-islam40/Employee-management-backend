f469e9de4dbd36de1be61c110c0c52555d58c22f
naveed-islam40 <naveedislam20ni@gmail.com>
2024-09-15 08:50:21 -0700

    change in cors

--- DB/dbConnect.js
+++ DB/dbConnect.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose")
 
+console.log(process.env.MONGO_URL || "Null")
+
 const dbConnect = () => {
     mongoose.connect(process.env.MONGO_URL).then(() => {
         console.log("mongodb connected successfully");
